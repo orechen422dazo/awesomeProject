b220f3c8541404cfc63aa812f03890c6e0e5de86
orechen422dazo <jboy422hobby@gmail.com>
2024-10-11 08:15:30 +0900

    wip

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "reflect"
+import (
+	"fmt"
+	"time"
+)
 
-type Foo struct {
-	Name string
+func sayHello(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
 }
 
 func main() {
-	var foo Foo
-	println(reflect.TypeOf(foo))
-	foo.Name = "foo"
+	go sayHello("world")
+	sayHello("hello")
 }
